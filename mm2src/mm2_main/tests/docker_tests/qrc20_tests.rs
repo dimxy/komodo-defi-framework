587b1aeb6f69686cce6b6d7af3a36a3e92844203
dimxy <dimxy@komodoplatform.com>
2024-06-17 22:24:57 +0500

    restore original test log message

--- mm2src/mm2_main/tests/docker_tests/qrc20_tests.rs
+++ mm2src/mm2_main/tests/docker_tests/qrc20_tests.rs
@@ -1048,8 +1048,8 @@ fn test_get_max_taker_vol_and_trade_with_dynamic_trade_fee(coin: QtumCoin, priv_
     // - `max_possible_2 = balance - locked_amount - max_trade_fee`, where `locked_amount = 0`
     let max_possible_2 = &qtum_balance - &max_trade_fee;
     // - `max_dex_fee = dex_fee(max_possible_2)`
-    let max_dex_fee = DexFee::new_from_taker_coin(&coin, "MYCOIN", &MmNumber::from(max_possible_2.clone()));
-    log!("max_dex_fee: {:?} fraction={:?}, max_possible_2={}", max_dex_fee, max_dex_fee.fee_amount().to_fraction(), max_possible_2);
+    let max_dex_fee = DexFee::new_from_taker_coin(&coin, "MYCOIN", &MmNumber::from(max_possible_2));
+    log!("max_dex_fee: {:?}", max_dex_fee.fee_amount().to_fraction());
 
     // - `max_fee_to_send_taker_fee = fee_to_send_taker_fee(max_dex_fee)`
     // `taker_fee` is sent using general withdraw, and the fee get be obtained from withdraw result
