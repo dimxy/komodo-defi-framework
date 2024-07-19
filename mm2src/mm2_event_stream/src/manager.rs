94621f3b23880ac4380ce982f8267fdb292175c0
Omer Yacine <mariocynicys@gmail.com>
2024-07-19 14:55:43 +0200

    ensure atomicity in streaming manager operations
    This refactor makes sure that we perform write operations on the streaming manager atomically.
    
    We were previouly locking some fields of the manager to edit them then unlock them and lock another field. This clearly isn't atomic since there is a depedency between the data stored in the two fields and they should be set in one step with no operation in between.
    
    One solution could have been to just wrap the whole manager with a mutex and not allow concurrent access to it. This would hurt the read perf though, which is critical for frequent send calls and broadcast calls.
    Another would have been introducing a new field (mutex) called `critical` or something and lock it when we want no other writing method to execute in parallel. This is the approach taken here, but since `clinets` field is already locked with every operation (every operation mutating the state of the streaming manager must involve a client anyway: added, removed, newly listening, etc...), we now lock the `clients` field before any cirtical section till the end so we are sure no other mutating code will run in parallel.

--- mm2src/mm2_event_stream/src/manager.rs
+++ mm2src/mm2_event_stream/src/manager.rs
@@ -4,6 +4,7 @@ use std::sync::{Arc, Mutex, RwLock};
 
 use crate::{Controller, Event, EventStreamer};
 use common::executor::abortable_queue::WeakSpawner;
+use common::log;
 
 use futures::channel::mpsc::UnboundedSender;
 use futures::channel::oneshot;
@@ -21,11 +22,11 @@ pub enum StreamingManagerError {
     /// Couldn't spawn the streamer.
     SpawnError(String),
     /// The client is not known/registered.
-    UnknownClient(u64),
+    UnknownClient,
     /// A client with the same ID already exists.
-    ClientExists(u64),
+    ClientExists,
     /// The client is already listening to the streamer.
-    ClientAlreadyListening(u64, String),
+    ClientAlreadyListening,
 }
 
 struct StreamerInfo {
@@ -56,6 +57,9 @@ pub struct StreamingManager {
     // Using an RwLock assuming `StreamingManager::broadcast` will be heavily used.
     controller: Arc<RwLock<Controller<Event>>>,
     /// An inverse map from clients to the streamers they are listening to.
+    /// Lists known clients and is always kept in sync with `streamers`.
+    // NOTE: Any operation writing to this field will also write to `streamers` and potentially `controller`.
+    // Make sure to lock this field first and all the way until the changes are made to other fields.
     clients: Arc<Mutex<HashMap<u64, HashSet<String>>>>,
 }
 
@@ -67,48 +71,66 @@ impl StreamingManager {
         streamer: impl EventStreamer,
         spawner: WeakSpawner,
     ) -> Result<(), StreamingManagerError> {
-        // Make sure we know this client.
-        if !self.clients.lock().unwrap().contains_key(&client_id) {
-            return Err(StreamingManagerError::UnknownClient(client_id));
-        }
         let streamer_id = streamer.streamer_id();
-        // If a streamer is already up and running, add the client to it and return.
-        if let Some(streamer_info) = self.streamers.write().unwrap().get_mut(&streamer_id) {
-            if streamer_info.clients.contains(&client_id) {
-                // FIXME: such info could have been known from `client_to_streamers` map
-                // check the atomicity of these operations (considering we are running inside an async context).
-                return Err(StreamingManagerError::ClientAlreadyListening(client_id, streamer_id));
+
+        // Pre-checks before spawning the streamer. Inside another scope to drop the locks early.
+        {
+            let mut clients = self.clients.lock().unwrap();
+            match clients.get(&client_id) {
+                // We don't know that client. We don't have a connection to it.
+                None => return Err(StreamingManagerError::UnknownClient),
+                // The client is already listening to that streamer.
+                Some(listening_to) if listening_to.contains(&streamer_id) => {
+                    return Err(StreamingManagerError::ClientAlreadyListening);
+                },
+                _ => (),
             }
-            streamer_info.clients.insert(client_id);
-            // FIXME: The streamer running is probably running with different configuration.
-            // We might want to inform the client that the configuration they asked for wasn't
-            // applied and return the active configuration instead?
-            // Note that there is no way to (re)spawn a streamer with a different configuration
-            // unless then we want to have different streamer for each and every client!
-            return Ok(());
+            // If a streamer is already up and running, we won't spawn another one.
+            if let Some(streamer_info) = self.streamers.write().unwrap().get_mut(&streamer_id) {
+                // Register the client as a listener to the streamer.
+                streamer_info.clients.insert(client_id);
+                // Register the streamer as listened-to by the client.
+                clients
+                    .get_mut(&client_id)
+                    .map(|listening_to| listening_to.insert(streamer_id));
+                // FIXME: The streamer running is probably running with different configuration.
+                // We might want to inform the client that the configuration they asked for wasn't
+                // applied and return the active configuration instead?
+                //
+                // Note that there is no way to (re)spawn a streamer with a different configuration
+                // unless then we want to have different streamer for each and every client!
+                //
+                // Another restricted solution is to not let clients set the streamer configuration
+                // and only allow it through MM2 configuration file.
+                return Ok(());
+            }
         }
-        // Otherwise spawn a new streamer.
+
+        // Spawn a new streamer.
         let (shutdown, data_in) = streamer
             .spawn(spawner, self.clone())
             .await
             .map_err(StreamingManagerError::SpawnError)?;
         let streamer_info = StreamerInfo::new(data_in, shutdown);
-        // Note that while we were spawning the streamer (potentially a long operation) and not holding the lock
-        // to ths streamers map (and we indeed shouldn't), another client might have added the same streamer.
-        // So we shouldn't assume that the streamer doesn't exist based solely on the check above.
-        self.streamers
-            .write()
-            .unwrap()
-            .entry(streamer_id.clone())
-            .or_insert(streamer_info)
-            .clients
-            .insert(client_id);
-        self.clients
-            .lock()
-            .unwrap()
-            .entry(client_id)
-            .or_insert(HashSet::new())
-            .insert(streamer_id);
+
+        // Note that we didn't hold the locks while spawning the streamer (potentially a long operation).
+        // This means we can't assume either that the client still exists at this point or
+        // that the streamer still doesn't exist.
+        let mut clients = self.clients.lock().unwrap();
+        if let Some(listening_to) = clients.get_mut(&client_id) {
+            listening_to.insert(streamer_id.clone());
+            self.streamers
+                .write()
+                .unwrap()
+                .entry(streamer_id.clone())
+                .or_insert(streamer_info)
+                .clients
+                .insert(client_id);
+        } else {
+            // The client was removed while we were spawning the streamer.
+            // We no longer have a connection for it.
+            return Err(StreamingManagerError::UnknownClient);
+        }
         Ok(())
     }
 
@@ -125,21 +147,29 @@ impl StreamingManager {
     }
 
     /// Stops streaming from the streamer with `streamer_id` to the client with `client_id`.
-    pub fn stop(&self, client_id: u64, streamer_id: &str) {
-        self.clients.lock().unwrap().get_mut(&client_id).map(|streamer_ids| {
-            streamer_ids.remove(streamer_id);
-        });
-        let mut streamers = self.streamers.write().unwrap();
-        streamers.get_mut(streamer_id).map(|info| {
-            info.clients.remove(&client_id);
-        });
-        // If there are no more listening clients, terminate the streamer.
-        if streamers.get(streamer_id).map(|info| info.clients.len()) == Some(0) {
-            streamers.remove(streamer_id);
+    pub fn stop(&self, client_id: u64, streamer_id: &str) -> Result<(), StreamingManagerError> {
+        let mut clients = self.clients.lock().unwrap();
+        if let Some(listening_to) = clients.get_mut(&client_id) {
+            listening_to.remove(streamer_id);
+
+            let mut streamers = self.streamers.write().unwrap();
+            streamers
+                .get_mut(streamer_id)
+                .ok_or(StreamingManagerError::StreamerNotFound)?
+                .clients
+                .remove(&client_id);
+
+            // If there are no more listening clients, terminate the streamer.
+            if streamers.get(streamer_id).map(|info| info.clients.len()) == Some(0) {
+                streamers.remove(streamer_id);
+            }
+            Ok(())
+        } else {
+            Err(StreamingManagerError::UnknownClient)
         }
     }
 
-    /// Broadcasts some event directly to listening clients.
+    /// Broadcasts some event to clients listening to it.
     ///
     /// In contrast to `StreamingManager::send`, which sends some data to a streamer,
     /// this method broadcasts an event to the listening *clients* directly, independently
@@ -162,33 +192,35 @@ impl StreamingManager {
     pub fn new_client(&self, client_id: u64) -> Result<Receiver<Arc<Event>>, StreamingManagerError> {
         let mut clients = self.clients.lock().unwrap();
         if clients.contains_key(&client_id) {
-            return Err(StreamingManagerError::ClientExists(client_id));
+            return Err(StreamingManagerError::ClientExists);
         }
         clients.insert(client_id, HashSet::new());
-        // Note that events queued in the channel are `Arc` shared.
-        // So 1024 long buffer won't be heavy on memory.
+        // Note that events queued in the channel are `Arc<` shared.
+        // So a 1024 long buffer isn't actually heavy on memory.
         Ok(self.controller.write().unwrap().create_channel(client_id, 1024))
     }
 
     pub fn remove_client(&self, client_id: u64) -> Result<(), StreamingManagerError> {
-        let streamer_ids = self
-            .clients
-            .lock()
-            .unwrap()
-            .remove(&client_id)
-            .ok_or(StreamingManagerError::UnknownClient(client_id))?;
-        self.controller.write().unwrap().remove_channel(&client_id);
-
+        let mut clients = self.clients.lock().unwrap();
+        // Remove the client from our known-clients map.
+        let listening_to = clients.remove(&client_id).ok_or(StreamingManagerError::UnknownClient)?;
+        // Remove the client from all the streamers it was listening to.
         let mut streamers = self.streamers.write().unwrap();
-        for streamer_id in streamer_ids {
+        for streamer_id in listening_to {
             if let Some(streamer_info) = streamers.get_mut(&streamer_id) {
                 streamer_info.clients.remove(&client_id);
-            };
+            } else {
+                log::error!(
+                    "Client {client_id} was listening to a non-existent streamer {streamer_id}. This is a bug!"
+                );
+            }
             // If there are no more listening clients, terminate the streamer.
             if streamers.get(&streamer_id).map(|info| info.clients.len()) == Some(0) {
                 streamers.remove(&streamer_id);
             }
         }
+        // Remove our channel with this client.
+        self.controller.write().unwrap().remove_channel(&client_id);
         Ok(())
     }
 }
