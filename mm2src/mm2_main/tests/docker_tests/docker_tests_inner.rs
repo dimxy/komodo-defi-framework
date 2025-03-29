6707f8c814c9207936549dad9d6dfc2fe0bba39e
dimxy <dimxy@komodoplatform.com>
2025-03-30 00:03:12 +0500

    fix test

--- mm2src/mm2_main/tests/docker_tests/docker_tests_inner.rs
+++ mm2src/mm2_main/tests/docker_tests/docker_tests_inner.rs
@@ -1716,11 +1716,11 @@ fn test_trade_preimage_not_sufficient_balance() {
     })))
     .unwrap();
     assert!(!rc.0.is_success(), "trade_preimage success, but should fail: {}", rc.1);
-    // balance(0.000015)
-    let available = MmNumber::from("0.000015").to_decimal();
-    // balance(0.000015) + transaction_fee(0.00001)
-    let required = MmNumber::from("0.00002").to_decimal();
-    expect_not_sufficient_balance(&rc.1, available, required, None);
+    // balance(0.00001273)
+    let available = MmNumber::from("0.00001273").to_decimal();
+    // required min_tx_amount(0.00001) + transaction_fee(0.00000274)
+    let required = MmNumber::from("0.00001274").to_decimal();
+    expect_not_sufficient_balance(&rc.1, available, required, Some(MmNumber::from("0").to_decimal()));
 
     fill_balance_functor(MmNumber::from("7.770085").to_decimal());
     let rc = block_on(mm.rpc(&json!({
@@ -1737,12 +1737,12 @@ fn test_trade_preimage_not_sufficient_balance() {
     })))
     .unwrap();
     assert!(!rc.0.is_success(), "trade_preimage success, but should fail: {}", rc.1);
-    let available = MmNumber::from("7.7701").to_decimal();
+    let available = MmNumber::from("7.77009773").to_decimal();
     // `required = volume + fee_to_send_taker_payment + dex_fee + fee_to_send_dex_fee`,
     // where `volume = 7.77`, `fee_to_send_taker_payment = fee_to_send_dex_fee = 0.00001`, `dex_fee = 0.01`.
     // Please note `dex_fee = 7.77 / 777` with dex_fee = 0.01
     // required = 7.77 + 0.01 (dex_fee) + (0.0001 * 2) = 7.78002
-    let required = MmNumber::from("7.78002");
+    let required = MmNumber::from("7.78000815");
     expect_not_sufficient_balance(&rc.1, available, required.to_decimal(), Some(BigDecimal::from(0)));
 }
 
