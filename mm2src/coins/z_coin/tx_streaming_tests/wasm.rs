d75ab611a83785aeb0ec3d1ca8527704cefbb5ee
dimxy <dimxy@komodoplatform.com>
2025-10-14 22:12:48 +0500

    fix page num

--- mm2src/coins/z_coin/tx_streaming_tests/wasm.rs
+++ mm2src/coins/z_coin/tx_streaming_tests/wasm.rs
@@ -115,6 +115,6 @@ async fn test_zcoin_tx_history() {
 
     println!("tx={:?}", tx);
     log!("tx={:?}", tx);*/
-    let r = fetch_tx_history_from_db(&coin, 1000, PagingOptionsEnum::default()).await;
+    let r = fetch_tx_history_from_db(&coin, 1000, PagingOptionsEnum::PageNumber(2)).await;
     log!("fetch_tx_history_from_db={:?}", r);
 }
